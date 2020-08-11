d084862701b7876822b75a5a7ad621e183dc006d
jnicewander <jnicewander@live.com>
2020-08-10 23:33:48 -0400

    Added http request methods functionality.

--- src/app/services/cart-api.service.ts
+++ src/app/services/cart-api.service.ts
@@ -1,21 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Item } from '../interfaces/item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  baseUrl: string = 'http://localhost:3000/cart-items';
+
+  // Add a getAllItems() method that uses http to make a GET request to your /cart-items API endpoint. 
   getAllItems() {
-    // Add a getAllItems() method that uses http to make a GET request to your /cart-items API endpoint. 
+    return this.http.get(this.baseUrl);
   }
 
-  deleteItem() {
-    // Add a deleteItem(id:number) method that uses http to make a DELETE request to your /cart-items/:id API endpoint.
+  // Add a deleteItem(id:number) method that uses http to make a DELETE request to your /cart-items/:id API endpoint.
+  deleteItem(id: number) {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete(url);
   }
 
-  addItem() {
-    // Add an addItem(item:Item) method that uses http to make a POST request to your /cart-items API endpoint.
+  // Add an addItem(item:Item) method that uses http to make a POST request to your /cart-items API endpoint.
+  addItem(item: Item) {
+    return this.http.post(this.baseUrl, item);
   }
 }
