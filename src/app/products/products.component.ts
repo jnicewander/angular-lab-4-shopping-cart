29e1bfeef7ee5a97c25e603e30fdb95c8fbb3f6c
J.Nicewander <jnicewander@live.com>
2020-08-11 12:33:10 -0400

    Added API request, and imports.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartApiService } from '../services/cart-api.service';
+import { Item } from '../interfaces/item';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: CartApiService) { }
+  
+  products: Item;
 
   ngOnInit(): void {
+    this.api.getAllItems().subscribe((res: Item) => {
+      this.products = res;
+    })
   }
 
 }
