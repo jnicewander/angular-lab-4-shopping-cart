668199ae05a3e6b36f9b4784d18f95480a4b86e6
J.Nicewander <jnicewander@live.com>
2020-08-12 13:49:41 -0400

    Added event emitter to send item properties from form as an object.

--- src/app/product-form/product-form.component.ts
+++ src/app/product-form/product-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Item } from '../interfaces/item';
 
 @Component({
   selector: 'app-product-form',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-form.component.css']
 })
 export class ProductFormComponent implements OnInit {
+  @Output() onSubmit = new EventEmitter<Item>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  submitForm(addProductForm: NgForm) {
+    this.onSubmit.emit({
+      product: addProductForm.value.product,
+      price: addProductForm.value.price,
+      quantity: addProductForm.value.quantity
+    });
+    addProductForm.reset();
+  }
 }
